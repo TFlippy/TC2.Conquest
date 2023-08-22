cc9f1ae4c631a811a72448ab5be38cd1d632e48e
TFlippy <TFlippy@users.noreply.github.com>
2023-08-22 11:53:04 +0200

    Camera snapping

--- content/Gamemode/WorldMap.cs
+++ content/Gamemode/WorldMap.cs
@@ -6,6 +6,7 @@ namespace TC2.Conquest
 	public static partial class WorldMap
 	{
 #if CLIENT
+		public static Vector2 worldmap_offset_current_snapped;
 		public static Vector2 worldmap_offset_current;
 		public static Vector2 worldmap_offset_target;
 		public static Vector2 momentum;
@@ -139,6 +140,10 @@ namespace TC2.Conquest
 			{
 				var rect = group_canvas.GetInnerRect();
 
+				var enable_momentum = true;
+				var enable_snapping = true;
+				var snap_camera = true;
+
 				//if (window.show)
 				//using (var button = GUI.CustomButton.New(id: "worldmap.button", size: GUI.Rm, set_cursor: false))
 				//GUI.ButtonBehavior("worldmap.button", rect: rect, out var hovered, out _);
@@ -149,7 +154,7 @@ namespace TC2.Conquest
 					//sb.Clear();
 
 					//var hovered = true;
-					var scale_canvas = GUI.GetWorldToCanvasScale();
+					//var scale_canvas = GUI.GetWorldToCanvasScale();
 					//var hovered = button.hovered;
 
 					var mouse = GUI.GetMouse();
@@ -174,6 +179,8 @@ namespace TC2.Conquest
 					using (GUI.Clip.Push(rect))
 					{
 						var scale_b = IWorld.WorldMap.scale_b;
+						var snap_delta = (worldmap_offset_current_snapped - worldmap_offset_current) * 1.0f;
+						//snap_delta = default;
 
 						//mat_proj = Matrix3x2.Identity;
 						//mat_proj.M11 = 2.00f / size.X;
@@ -183,15 +190,18 @@ namespace TC2.Conquest
 						//var mat_vp = Matrix3x2.Multiply(mat_view, mat_proj);
 						//Matrix3x2.Invert(mat_vp, out var mat_vp_inv);
 
-						var mat_l2c = Maths.TRS3x2((worldmap_offset_current * -zoom) + rect.GetPosition(new(0.50f)), rotation, new Vector2(zoom));
+						var mat_l2c = Maths.TRS3x2((worldmap_offset_current * -zoom) + rect.GetPosition(new(0.50f)) - snap_delta, rotation, new Vector2(zoom));
 						Matrix3x2.Invert(mat_l2c, out var mat_c2l);
 
-						var mat_l2c2 = Maths.TRS3x2(rect.GetPosition(new(0.50f)), rotation, new Vector2(1));
+						var mat_l2c2 = Maths.TRS3x2(rect.GetPosition(new Vector2(0.50f)), rotation, new Vector2(1));
 						Matrix3x2.Invert(mat_l2c2, out var mat_c2l2);
+
+						//var snap_delta_canvas = Vector2.Transform(snap_delta, mat_l2c);
+						var snap_delta_canvas = snap_delta * zoom;
 
 						//mat_l2c.Translation += rect.GetPosition(new(0.50f));
 
-						var uv_offset = worldmap_offset_current / scale_canvas;
+						//var uv_offset = worldmap_offset_current_snapped / scale_canvas;
 
 						var tex_scale = use_renderer ? (IWorld.WorldMap.worldmap_size.X / scale_b) * zoom : 16.00f;
 						var tex_scale_inv = 1.00f / tex_scale;
@@ -204,7 +214,7 @@ namespace TC2.Conquest
 						{
 							//GUI.DrawTexture("_worldmap", rect, GUI.Layer.Window, uv_0: (Vector2.Transform(rect.a, mat_c2l2) * tex_scale_inv) - new Vector2(0.50f), uv_1: (Vector2.Transform(rect.b, mat_c2l2) * tex_scale_inv) - new Vector2(0.50f), clip: false);
 							//GUI.DrawTexture("_worldmap", rect, GUI.Layer.Window, uv_0: (Vector2.Transform(rect.a, mat_c2l2) * tex_scale_inv) + new Vector2(0.50f), uv_1: (Vector2.Transform(rect.b, mat_c2l2) * tex_scale_inv) + new Vector2(0.50f), clip: false);
-							GUI.DrawTexture("_worldmap", rect, GUI.Layer.Window, uv_0: (Vector2.Transform(rect.a, mat_c2l2) * tex_scale_inv) + new Vector2(0.50f), uv_1: (Vector2.Transform(rect.b, mat_c2l2) * tex_scale_inv) + new Vector2(0.50f), clip: false);
+							GUI.DrawTexture("_worldmap", rect, GUI.Layer.Window, uv_0: (Vector2.Transform(rect.a - snap_delta_canvas, mat_c2l2) * tex_scale_inv) + new Vector2(0.50f), uv_1: (Vector2.Transform(rect.b - snap_delta_canvas, mat_c2l2) * tex_scale_inv) + new Vector2(0.50f), clip: false);
 						}
 						else
 						{
@@ -218,7 +228,7 @@ namespace TC2.Conquest
 						var mouse_pos = GUI.GetMousePosition(); // Vector2.Transform(GUI.GetMousePosition(), mat_c2l);
 						var mouse_local = Vector2.Transform(mouse_pos, mat_c2l);
 						var mouse_local_snapped = mouse_local;
-						mouse_local_snapped.Snap(1, out mouse_local_snapped);
+						mouse_local_snapped.Snap(1.00f / scale_b, out mouse_local_snapped);
 
 						var tex_line_district = h_texture_line_00;
 						var tex_line_province = h_texture_line_01;
@@ -506,8 +516,8 @@ namespace TC2.Conquest
 
 						#region Overlays
 						GUI.DrawTexture(GUI.tex_vignette, rect, layer: GUI.Layer.Window, color: Color32BGRA.White.WithAlphaMult(0.30f));
-						GUI.DrawSpriteCentered(new Sprite(h_texture_icons, 72, 72, 0, 1), rect: AABB.Centered(Vector2.Transform(mouse_local_snapped, mat_l2c), new Vector2(0.25f)), layer: GUI.Layer.Window, scale: 0.125f * 0.250f * zoom, color: Color32BGRA.Black.WithAlphaMult(0.20f));
-						GUI.DrawTextCentered($"Zoom: {zoom:0.00}x\n[{mouse_local_snapped.X:0}, {mouse_local_snapped.Y:0}]\n[{worldmap_offset_target.X:0.00}, {worldmap_offset_target.Y:0.00}]\n[{mouse_local.X:0.00}, {mouse_local.Y:0.00}]\n[{mouse_pos.X:0.00}, {mouse_pos.Y:0.00}]", position: rect.GetPosition(new(1, 1)), new(1, 1), font: GUI.Font.Superstar, size: 24, layer: GUI.Layer.Foreground);
+						GUI.DrawSpriteCentered(new Sprite(h_texture_icons, 72, 72, 0, 1), rect: AABB.Centered(Vector2.Transform(mouse_local_snapped, mat_l2c), new Vector2(0.25f)), layer: GUI.Layer.Window, scale: 0.125f * 0.50f * zoom, color: Color32BGRA.Black.WithAlphaMult(1));
+						GUI.DrawTextCentered($"Zoom: {zoom:0.00}x\ndelta: [{snap_delta.X:0.000000}, {snap_delta.Y:0.000000}]\ndelta.c: [{snap_delta_canvas.X:0.000000}, {snap_delta_canvas.Y:0.000000}]\ncam: [{worldmap_offset_target.X:0.000000}, {worldmap_offset_target.Y:0.000000}]\ncam.s: [{worldmap_offset_current_snapped.X:0.000000}, {worldmap_offset_current_snapped.Y:0.000000}]\nmouse.l: [{mouse_local.X:0.0000}, {mouse_local.Y:0.0000}]\nmouse: [{mouse_pos.X:0.00}, {mouse_pos.Y:0.00}]", position: rect.GetPosition(new(1, 1)), new(1, 1), font: GUI.Font.Superstar, size: 24, layer: GUI.Layer.Foreground);
 						#endregion
 
 						#region Editor
@@ -557,7 +567,7 @@ namespace TC2.Conquest
 						{
 							//worldmap_offset += mouse_delta * zoom_inv;
 							//momentum = mouse_delta; // Vector2.Lerp(momentum, mouse_delta, 0.80f); // mouse_delta.LengthSquared() > 0.50f || mouse.GetKeyUp(Mouse.Key.Left) ? Vector2.Lerp(momentum, mouse_delta, 0.50f) : Vector2.Zero;
-							momentum = Vector2.Lerp(momentum, mouse_delta, 0.50f); // mouse_delta.LengthSquared() > 0.50f || mouse.GetKeyUp(Mouse.Key.Left) ? Vector2.Lerp(momentum, mouse_delta, 0.50f) : Vector2.Zero;
+							if (enable_momentum) momentum = Vector2.Lerp(momentum, mouse_delta, 0.50f); // mouse_delta.LengthSquared() > 0.50f || mouse.GetKeyUp(Mouse.Key.Left) ? Vector2.Lerp(momentum, mouse_delta, 0.50f) : Vector2.Zero;
 							worldmap_offset_target += (mouse_delta * zoom_inv); // + (momentum * zoom_inv * 0.10f);
 							worldmap_offset_current = Vector2.Lerp(worldmap_offset_current, worldmap_offset_target, 0.25f);
 
@@ -565,23 +575,25 @@ namespace TC2.Conquest
 						}
 						else
 						{
-							momentum = Vector2.Lerp(momentum, Vector2.Zero, 0.10f);
+							if (enable_momentum) momentum = Vector2.Lerp(momentum, Vector2.Zero, 0.10f);
 							worldmap_offset_target += momentum * zoom_inv;
 							worldmap_offset_current = Vector2.Lerp(worldmap_offset_current, worldmap_offset_target, 0.25f);
+							if (!enable_momentum) momentum = default;
 						}
 
 						if (hovered)
 						{
 							worldmap_zoom_target -= mouse.GetScroll(0.25f);
 							//worldmap_zoom = Maths.Clamp(worldmap_zoom, 1.00f, 8.00f);
-							worldmap_zoom_target = Maths.Clamp(worldmap_zoom_target, 1.00f, 6.50f);
+							worldmap_zoom_target = Maths.Clamp(worldmap_zoom_target, 1.00f, 7.00f);
 						}
 
 						worldmap_zoom_current = Maths.Lerp(worldmap_zoom_current, worldmap_zoom_target, 0.20f);
 
 						if (use_renderer)
 						{
-							IWorld.WorldMap.Renderer.UpdateCamera(worldmap_offset_current, 0.00f, new Vector2(1));
+							worldmap_offset_current_snapped = Maths.SnapFloor(worldmap_offset_current, 1 / scale_b);
+							IWorld.WorldMap.Renderer.UpdateCamera(worldmap_offset_current_snapped, 0.00f, new Vector2(1));
 
 							ref var world_data = ref h_world.GetData();
 							if (world_data.IsNotNull())
@@ -600,7 +612,10 @@ namespace TC2.Conquest
 						{
 							if (kb.GetKeyDown(Keyboard.Key.Reload))
 							{
+								worldmap_offset_current = default;
+								worldmap_offset_current_snapped = default;
 								worldmap_offset_target = default;
+								momentum = default;
 								rotation = default;
 							}
 
