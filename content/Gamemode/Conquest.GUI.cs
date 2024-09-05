76a0fbf38ede10cef326f0373eed57c91503234d
TFlippy <TFlippy@users.noreply.github.com>
2024-09-05 16:49:36 +0200

    GUI

--- content/Gamemode/Conquest.GUI.cs
+++ content/Gamemode/Conquest.GUI.cs
@@ -48,7 +48,7 @@ namespace TC2.Conquest
 
 				Spawn.RespawnGUI.window_size = new Vector2(580, 480);
 
-				using (var window = GUI.Window.Standalone("Respawn"u8, position: new Vector2(GUI.CanvasSize.X * 0.40f, 0) + Spawn.RespawnGUI.window_offset, pivot: Spawn.RespawnGUI.window_pivot, size: Spawn.RespawnGUI.window_size, flags: GUI.Window.Flags.No_Appear_Focus | GUI.Window.Flags.No_Click_Focus))
+				using (var window = GUI.Window.Standalone("Respawn"u8, position: new Vector2(GUI.CanvasSize.X * 0.40f, 0) + Spawn.RespawnGUI.window_offset, pivot: Spawn.RespawnGUI.window_pivot, size: Spawn.RespawnGUI.window_size, flags: GUI.Window.Flags.No_Appear_Focus))
 				{
 					this.StoreCurrentWindowTypeID();
 					if (window.show)
